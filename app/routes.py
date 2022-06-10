61d522cc3c7ef2301046b985ad205a76611de0ae
dmitriy-kochetkov <dmi3y.kochetkov@gmail.com>
2022-06-10 11:35:04 +0300

    favicon url fix

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def before_request():
         db.session.commit()
 
 
-@app.route('favicon.ico')
+@app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.png', mimetype='image/vnd.microsoft.icon')
 
