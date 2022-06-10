105843ec9564aee101d7896931bb26454d3773ed
dmitriy-kochetkov <dmi3y.kochetkov@gmail.com>
2022-06-10 16:36:24 +0300

    deploy

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -83,8 +83,21 @@ function changeLetterTypeHandler(event) {
     updateSendButtonState()
 }
 
+// получаем координаты элемента в контексте документа
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset
+    };
+  }
+
 function undefinedLetterMouseHover(event) {
-    targetBCR = event.target.getBoundingClientRect()            
+    //targetBCR = event.target.getBoundingClientRect()
+    targetBCR = getCoords(event.target)       
 
     let popup = document.querySelector('#popup-0')
     popup.style.display = 'flex';
