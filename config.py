1713d2a5a287447c11778cb5f6ee089ce6cad9b6
dmitriy-kochetkov <dmi3y.kochetkov@gmail.com>
2022-06-10 13:21:12 +0300

    Config logging to stdout

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'pew-pew-pew-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
